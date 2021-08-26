76475669867ec7613218451d4ee9b4ff960e36d9
czw <419627345@qq.com>
2021-08-26 16:20:21 +0800

    注释

--- lib/src/FlutterYs7.dart
+++ lib/src/FlutterYs7.dart
@@ -8,15 +8,12 @@ class FlutterYs7 {
 
   static const _plugin_channel = const MethodChannel("cspy/flutter_ys7/plugin");
 
-  // static Future<String> get platformVersion async {
-  //   final String version = await _channel.invokeMethod('getPlatformVersion');
-  //   return version;
-  // }
-
+  //  *  @param token AccessToken
+  //  *  @param deviceSerial 设备序列号
+  //  *  @param verifyCode   密码
+  //  *  @param cameraNO   通道
   static Future<bool> startVideo4(String token, String deviceSerial,
       String verifyCode, int cameraNO) async {
-    //  *  @param deviceSerial 设备序列号
-    //  *  @param cameraNo     通道号
     var result = await _channel.invokeMethod("start", {
       'token': token,
       'deviceSerial': deviceSerial,
@@ -27,7 +24,7 @@ class FlutterYs7 {
     return true;
   }
 
-  //  *  @param token token
+  //  *  @param token AccessToken
   //  *  @param deviceSerial 设备序列号
   //  *  @param verifyCode   密码
   static Future<bool> startVideo3(
@@ -35,6 +32,8 @@ class FlutterYs7 {
     return await startVideo4(token, deviceSerial, verifyCode, 1);
   }
 
+  //  *  @param token AccessToken
+  //  *  @param deviceSerial 设备序列号
   static Future<bool> startVideo2(
       String token,
       String deviceSerial,
@@ -42,6 +41,7 @@ class FlutterYs7 {
     return await startVideo4(token, deviceSerial, '', 1);
   }
 
+  //  *  @param appKey 账号appKey
   static Future<bool> initSdk(String appKey) async {
     var result = await _plugin_channel.invokeMethod("init_sdk", {
       'appKey': appKey,
